2fe9bea1ebf3a0683ece9aeca6c062888dda8858
Mauro Chojrin <mauro.chojrin@codurance.com>
2022-07-27 10:54:34 +0200

    Acceptance test failing for the right reason

--- src/main/java/com/codurance/todo/TaskListImplementation.java
+++ src/main/java/com/codurance/todo/TaskListImplementation.java
@@ -3,15 +3,15 @@ package com.codurance.todo;
 public class TaskListImplementation implements TaskList {
     @Override
     public void addTask(String description) {
-        throw new UnsupportedOperationException();
+//        throw new UnsupportedOperationException();
     }
 
     @Override
     public void completeTask(String description) {
-        throw new UnsupportedOperationException();
+//        throw new UnsupportedOperationException();
     }
 
     public void showTasks() {
-        throw new UnsupportedOperationException();
+//        throw new UnsupportedOperationException();
     }
 }
