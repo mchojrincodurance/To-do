461b8668d1f8030ad6f1249f54ace3335e9c4498
Mauro Chojrin <mauro.chojrin@codurance.com>
2022-07-27 13:23:22 +0200

    Improving starting point

--- src/main/java/TaskList.java
+++
@@ -1,7 +0,0 @@
-public interface TaskList {
-    void addTask(String description);
-
-    void completeTask(TaskId id);
-
-    void showTasks();
-}
---
+++ src/main/java/TaskListInterface.java
@@ -0,0 +1,7 @@
+public interface TaskListInterface {
+    void addTask(String description);
+
+    void completeTask(String description);
+
+    void showTasks();
+}
