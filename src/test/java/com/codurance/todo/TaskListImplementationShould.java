d81f1291ecf374ae06df9726422e1a74375ecf10
Mauro Chojrin <mauro.chojrin@codurance.com>
2022-07-27 11:15:18 +0200

    Leveraging Mockito for easier test writing

--- src/test/java/com/codurance/todo/TaskListImplementationShould.java
+++ src/test/java/com/codurance/todo/TaskListImplementationShould.java
@@ -1,18 +1,23 @@
 package com.codurance.todo;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
+@ExtendWith(MockitoExtension.class)
 class TaskListImplementationShould {
 
     public static final String FIRST_TASK = "A task";
+    @Mock
+    TaskRepository taskRepository;
 
     @Test
     public void allow_adding_task() {
-        TaskRepository taskRepository = mock(TaskRepository.class);
         Task task = new Task(FIRST_TASK);
 
         TaskListImplementation taskList = new TaskListImplementation();
