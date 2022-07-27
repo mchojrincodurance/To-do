a57d0d2e9ae104766e7745bff1296117ade55212
Mauro Chojrin <mauro.chojrin@codurance.com>
2022-07-27 15:08:53 +0200

    Test for task.toString

--- src/test/java/com/codurance/todo/TaskShould.java
+++ src/test/java/com/codurance/todo/TaskShould.java
@@ -6,18 +6,38 @@ import org.junit.jupiter.params.provider.CsvSource;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class TaskShould {
+
+    public static final String TASK_DESCRIPTION = "A task";
+
     @ParameterizedTest
     @CsvSource({
             "true",
             "false"
     })
     public void know_when_its_completed(boolean isCompleted) {
-        Task task = new Task("a task");
+        Task task = buildTask(isCompleted);
+
+        assertEquals(isCompleted, task.isCompleted());
+    }
+
+    private static Task buildTask(boolean isCompleted) {
+        Task task = new Task(TASK_DESCRIPTION);
 
         if (isCompleted) {
             task.complete();
         }
 
-        assertEquals(isCompleted, task.isCompleted());
+        return task;
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "true",
+            "false"
+    })
+    public void convert_itself_to_string(boolean isCompleted) {
+        Task task = buildTask(isCompleted);
+
+        assertEquals(TASK_DESCRIPTION + "|" + (isCompleted ? "x" : ""), task.toString());
     }
 }
